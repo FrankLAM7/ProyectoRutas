8c47dfa2784c7456bc4c2e5e0ef44f9f8eb6676f
Josue Patricio <josretamozovar@gmail.com>
2019-07-19 13:27:22 -0500

    login funcional

--- index/index.js
+++ index/index.js
@@ -8,132 +8,93 @@ import {
 
 window.onload = () => {
 
+
     firebase.initializeApp(firebaseConfig);
-
-
-    let crearCuenta = () => {
-        let email = $("#inputEmail").val().trim();
-        let password = $("#inputPassword").val().trim();
-
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then(() => {
-                //$("#modalCrearCuenta").modal("hide");
-
-                Swal.fire({
-                    type: 'success',
-                    title: 'Exito ',
-                    text: 'Se creo el usuario!',
-                    //footer: '<a href>Why do I have this issue?</a>'
-                })
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    };
-
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(() => {
-            // abrir la siguiente pagina
-
-
-        })
-        .catch(function (error) {
-            //console.log(error);
-        });
-
-    let cerrarSesion = (e) => {
-        e.preventDefault();
-        firebase.auth().signOut()
-            .then(() => {
-                // redireccionar al index cuando la sesión se cierra
-
-                Swal.fire({
-                    position: 'top-end',
-                    type: 'success',
-                    title: 'Se ha cerrado la sesion',
-                    showConfirmButton: false,
-                    timer: 1500
-                })
-                location = "./index.html";
-            })
-            .catch(() => {
-
-            });
-    }
 
     /**
      * Función para inciar sesión con Firebase
      */
-    let iniciarSesion = () => {
-        let email = $("#inputEmail").val().trim();
-        let password = $("#inputPassword").val().trim();
+    let iniciarSesion = (e) => {
+        console.log(e);
+
+        e.preventDefault();
+        let email = $("#inputEmail").val();
+        let password = $("#inputPassword").val();
+        console.log(email, password);
 
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(() => {
                 //$("#modalIniciarSesion").modal("hide");
+                console.log("se inicio sesion");
+                Swal.fire({
+                    position: 'top-end',
+                    type: 'success',
+                    title: 'Se inicio la sesion',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+
             })
             .catch(function (error) {
                 // Handle Errors here.
                 var errorCode = error.code;
                 var errorMessage = error.message;
                 // ...
+                Swal.fire({
+                    position: 'top-end',
+                    type: 'error',
+                    title: 'Error',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+
             });
     }
+    //iniciarSesion();
 
 
     let verificarSesion = () => {
+
+
         firebase.auth().onAuthStateChanged(function (user) {
             if (user) {
                 console.log("Habia una sesion iniciada");
                 console.log(user);
-                
+
 
                 // VAMOS A VER SI EL USUARIO YA ESTABA REGISTRADO EN LA
                 // BASE DE DATOS EN TIEMPO REAL para crearlo o no crearlo
 
-                refUsuarios.on("value", dataSnapshot => {
-                    // existe => variable booleana
-                    // true si el nodo "usuarios" ya tenia ese child
-                    // false si el nodo "usuarios" no tenia ese child
-                    let existe = dataSnapshot.hasChild(user.uid);
-                    if (!existe) {
-                        // crear al usuario en la realtimeDatabase
-                        refUsuarios.child(user.uid).set({
-                            email: user.email
-                        }).then(() => {
-                            Swal.fire({
-                                position: 'top-end',
-                                type: 'success',
-                                title: 'Se ha cerrado la sesion',
-                                showConfirmButton: false,
-                                timer: 1500
-                            })
-                        })
-                    }
-                });
+                console.log("se inicio sesion");
 
+                console.log("se inicio sesion");
+                Swal.fire({
+                    position: 'top-end',
+                    type: 'success',
+                    title: 'Se inicio la sesion',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+                console.log(email, password);
 
             } else {
                 console.log("No habia una sesion iniciada o el usuario cerró sesion");
-                
 
-                
+                //console.log(email,password);
+
+
+
             }
         });
     }
 
+
+
     verificarSesion();
 
+    let boton = document.getElementById("btnIniciarSesion");
+    boton.onclick= iniciarSesion;
 
-   
-    $("#inputPassword").keyup(function (e) {
-        
-
-        
-    });
-
-   
 
 
 }
-
